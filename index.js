93d11f187bda08d8026c2e49ab6198439d6cdcf0
alexanderkozhevin <turboasm21@gmail.com>
2016-05-28 10:42:18 +0300

    fix8 test2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
 var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
 
 app.set('port', (process.env.PORT || 80));
 
--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   console.log('superman3');
-  var socket = io.connect('http://damp-waters-91942.herokuapp.com');
+  var socket = io.connect('https://damp-waters-91942.herokuapp.com', {secure: true});
   console.log('superman2');
   socket.on('connect', function () {
     console.log('superman');
