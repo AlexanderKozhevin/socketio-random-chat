ae65fd2c2e5bf203aab1de388137861b3fa9eefe
alexanderkozhevin <turboasm21@gmail.com>
2016-05-28 11:00:03 +0300

    fix12 test2

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var PORT = process.env.PORT || 8080;
 
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-
-
-
-
-app.set('port', (process.env.PORT || 80));
-
-//We don't really need this line, but it allows to send
-//requests from different domains
-app.use(cors());
-app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-
-
-app.get('/', function(request, response) {
-  response.sendfile('index.html');
+app.get('/',function(req,res){
+	//request : son cabeceras y datos que nos envia el navegador.
+	//response : son todo lo que enviamos desde el servidor.
+	res.sendFile(__dirname + 'index.html');
 });
 
+io.on('connection',function(socket){
+	console.log("usuario id : %s",socket.id);
 
+	// var channel = 'channel-a';
+  //
+	// socket.broadcast.emit('message','El usuario '+socket.id+' se ha conectado!','System');
+  //
+	// socket.join(channel);
+  //
+	// socket.on('message',function(msj){
+	// 	//io.emit('message',msj,socket.id);
+	// 	io.sockets.in(channel).emit('message',msj,socket.id); //enviar a todos del canal
+	// 	//socket.broadcast.to(channel).emit('message',msj,socket.id); //enviar a todos del canal menos a mi
+	// });
+  //
+	// socket.on('disconnect',function(){
+	// 	console.log("Desconectado : %s",socket.id);
+	// });
+  //
+	// socket.on('change channel',function(newChannel){
+	// 	socket.leave(channel);
+	// 	socket.join(newChannel);
+	// 	channel = newChannel;
+	// 	socket.emit('change channel',newChannel);
+	// });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
 });
 
-
-io.on('connection', function (socket) {
-  socket.on('hello', function (data) {
-    console.log(data);
-  });
+http.listen(PORT,function(){
+	console.log('el servidor esta escuchando el puerto %s',PORT);
 });
--- public/app.js
+++ public/app.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
   console.log('superman3');
   var socket = io.connect('https://damp-waters-91942.herokuapp.com', {secure: true});
-  io.set('transports', ['xhr-polling']);
-  io.set('polling duration', 10);
-  console.log('superman2');
+
   socket.on('connect', function () {
     console.log('superman');
     socket.emit('hello', { my: 'super test 1' });
