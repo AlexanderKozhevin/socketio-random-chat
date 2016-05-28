4cfff92a6ef8230ceb5ed5e4410ef6ba5a97255a
alexanderkozhevin <turboasm21@gmail.com>
2016-05-28 13:58:17 +0300

    test3 fix6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var PORT = process.env.PORT || 8080;
@@ -7,6 +8,7 @@ var PORT = process.env.PORT || 8080;
 var users = [];
 
 app.use(express.static(__dirname + '/public'));
+app.use(cors());
 
 app.get('/',function(req,res){
 
