ac785ea6e3499f3900801810177716d26e006f8a
alexanderkozhevin <turboasm21@gmail.com>
2016-05-28 10:38:32 +0300

    fix6 test2

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
 io.on('connection', function (socket) {
   socket.on('hello', function (data) {
     console.log(data);
--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-
-  var socket = io.connect('https://damp-waters-91942.herokuapp.com/');
-
+  console.log('superman3');
+  var socket = io.connect('https://damp-waters-91942.herokuapp.com', {secure: true});
+  console.log('superman2');
   socket.on('connect', function () {
+    console.log('superman');
     socket.emit('hello', { my: 'super test 1' });
   });
 })
