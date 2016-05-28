8e1e713b1fb3eb77bc3416a0a3a5fb7c98d2e457
alexanderkozhevin <turboasm21@gmail.com>
2016-05-28 14:07:17 +0300

    test3 fix7

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-  console.log('superman3');
-  var socket = io.connect('https://damp-waters-91942.herokuapp.com', {secure: true});
+var username = undefined;
+var socket;
+function newUser(){
+  username = document.querySelector('#username').value;
+  $('.nameinput').hide()
+}
 
-  socket.on('connect', function () {
-    console.log('superman');
-    socket.emit('hello', { my: 'super test 1' });
-  });
+$(document).ready(function(){
+  socket = io.connect('https://damp-waters-91942.herokuapp.com', {secure: true});
 })
