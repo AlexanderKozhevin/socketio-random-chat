7510eb6823e7d9aaaeb52bf9408c4aaa5d2638fb
alexanderkozhevin <turboasm21@gmail.com>
2016-05-28 10:20:04 +0300

    test2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
-
+var io = require('socket.io')(app);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,14 +10,19 @@ app.set('port', (process.env.PORT || 5000));
 //requests from different domains
 app.use(cors());
 app.use(bodyParser.json());
-
-
 app.use(express.static(__dirname + '/public'));
+
+
 
 app.get('/', function(request, response) {
   response.sendfile('index.html');
 });
 
+io.on('connection', function (socket) {
+  socket.on('hello', function (data) {
+    console.log(data);
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
--- public/app.js
+++ public/app.js
@@ -1,1 +1,6 @@
-console.log('random socket.io chat');
+$(document).ready(function(){
+
+  var socket = io.connect('https://damp-waters-91942.herokuapp.com/');
+  socket.emit('hello', { my: 'super test 1' });
+
+})
