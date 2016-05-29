4a7fbefd2ca811ee4d6b788fe3b0b02aad2f621d
alexanderkozhevin <turboasm21@gmail.com>
2016-05-29 05:16:39 +0300

    stage3-23

--- public/app.js
+++ public/app.js
@@ -2,46 +2,48 @@ var username = "JamesBond";
 var socket;
 var partner;
 
+function newUser(){
+  username = document.querySelector('#username').value;
+  $('.nameinput').hide()
+  $('.content').css('display', 'flex');
+  socket = io.connect('https://damp-waters-91942.herokuapp.com', {secure: true});
+
+  socket.on('status', function(data){
+    if (data.status=='pending'){
+      $('.messages').hide()
+      $('.input-container').hide()
+      $('.waiting').css('display', 'flex');
+      var myNode = document.querySelector(".messages");
+      while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+      }
+
+    }
+    if (data.status=='connected'){
+      $('.messages').show();
+      $('.input-container').css('display', 'flex');
+      $('.waiting').hide()
+      partner = data.partner;
+
+    }
+
+  })
+
+
+  socket.on('message', function(data){
+
+    var html = "<li><strong>" + data.partnerName + "</strong> - " + data.message + "</li>";
+    $('.messages').append(html)
+    $('.messages').animate({scrollTop: $('.messages').get(0).scrollHeight}, 500);
+
+
+  })
+}
+
 $(document).ready(function(){
 
 
-  function newUser(){
-    username = document.querySelector('#username').value;
-    $('.nameinput').hide()
-    $('.content').css('display', 'flex');
-    socket = io.connect('https://damp-waters-91942.herokuapp.com', {secure: true});
 
-    socket.on('status', function(data){
-      if (data.status=='pending'){
-        $('.messages').hide()
-        $('.input-container').hide()
-        $('.waiting').css('display', 'flex');
-        var myNode = document.querySelector(".messages");
-        while (myNode.firstChild) {
-          myNode.removeChild(myNode.firstChild);
-        }
-
-      }
-      if (data.status=='connected'){
-        $('.messages').show();
-        $('.input-container').css('display', 'flex');
-        $('.waiting').hide()
-        partner = data.partner;
-
-      }
-
-    })
-
-
-    socket.on('message', function(data){
-
-      var html = "<li><strong>" + data.partnerName + "</strong> - " + data.message + "</li>";
-      $('.messages').append(html)
-      $('.messages').animate({scrollTop: $('.messages').get(0).scrollHeight}, 500);
-
-
-    })
-  }
 
 
 })
