496aab0975a8fd6ce8ce2c22741e968bf937439c
alexanderkozhevin <turboasm21@gmail.com>
2016-05-29 03:53:35 +0300

    test super 1

--- public/app.js
+++ public/app.js
@@ -1,10 +1,32 @@
 var username = undefined;
 var socket;
+var partner;
 function newUser(){
   username = document.querySelector('#username').value;
   $('.nameinput').hide()
+  $('.content').show()
 }
 
 $(document).ready(function(){
   socket = io.connect('https://damp-waters-91942.herokuapp.com', {secure: true});
+
+  socket.on('status', function(data){
+    console.log('alskdjflajsklj')
+    if (data.status=='pending'){
+      $('.messages').hide()
+      $('.input-container').hide()
+      $('.waiting').show()
+    }
+    if (data.status=='connected'){
+      $('.messages').show()
+      $('.input-container').show()
+      $('.waiting').hide()
+      partner = data.partner;
+    }
+
+  })
+
+  function send(){
+
+  }
 })
