e01835e5514f7f8fc7a2e412007389dda4715f59
alexanderkozhevin <turboasm21@gmail.com>
2016-08-06 21:14:56 +0300

    fix

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function connectClients(clientID) {
 //Initialise user connection
 io.on('connection',function(socket){
 	console.log('* * *')
-	console.log(socket.handshake.headers.query.user)
+	console.log(socket.handshake)
 	console.log('* * *')
   //
   // Block 1 - Add new user to the common list
